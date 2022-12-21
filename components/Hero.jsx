68e1bf82b06a98112858db9ef8d5366ed55c28e4
JoshMuasya <muasyajoshua07@gmail.com>
2022-12-22 00:14:38 +0300

    Made changes on Landing Page

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,53 +1,54 @@
 import React from 'react';
 import Image from 'next/image';
+import logo1 from '../public/Logo1.png';
 
 const Hero = () => {
   return (
     // Background
-    <div className='flex h-screen bg-fixed bg-cover herosection'>
-      {/* Overlay*/}
-      <div className='absolute top-0 bottom-0 left-0 right-0 h-screen bg-herobg'>
-        <div className='flex flex-wrap justify-center content-center'>
-          <div className='absolute z-10 flex flex-row flex-wrap justify-around content-center items-center inset-y-44'>
-            {/* Logo */}
-            <div className='pr-32'>
-              <Image 
-                src="/Logo1.png"
-                alt="Logo"
-                width="324"
-                height="136"
-              />
-            </div>
+    <div className='h-screen bg-fixed bg-cover herosection'>
+      {/* Overlay */}
+      <div className='absolute top-0 bottom-0 left-0 right-0 h-screen bg-herobg' />
+      <div className='absolute z-10 w-full h-screen flex flex-wrap flex-col text-justify xl:flex-row justify-center content-center items-center'>
+        {/* Logo */}
+        <div className='w-1/4 xl:w-96 xl:pr-32 pb-7 xl:pb-0'>
+          <Image 
+            src={logo1}
+            alt="Logo"
+          />
+        </div>
 
-            {/* Details */}
-            <div className='flex flex-col flex-wrap'>
-              <div>
-                <h3 className='text-xl font-semibold'>Welcome to</h3>
-              </div>
-              <div>
-                <h1 className='font-black text-xxl'><span className='text-red'>MELLO</span> CANINE</h1>
-              </div>
-              <div>
-                <h4 className='font-medium text-lg'>
-                  Where, <br />
-                  we make bad Dogs Good <br />
-                  and Good Dogs Better <br />
-                </h4>
-              </div>
+        {/* Details */}
+        <div className='flex flex-col flex-wrap justify-center content-center items-center xl:items-start'>
+          <div>
+            <h3 className='text-sl pb-7 xl:pb-3 xl:text-xl font-semibold'>Welcome to</h3>
+          </div>
+          <div className='flex flex-col pb-7 xl:pb-3 lg:flex-row'>
+            <h1 className='font-black text-l xl:text-xxl text-red xl:pb-0 lg:pr-5'>MELLO</h1>
+            <h1 className='font-black text-l xl:text-xxl xl:pt-0'>CANINE</h1>
+          </div>
+          <div className='flex flex-col pb-7 xl:pb-3 justify-center content-center items-center xl:items-start'>
+            <h4 className='text-justify font-medium text-s xl:text-l'>
+              Where,
+            </h4>
+            <h4 className='text-justify font-medium text-s xl:text-l'>
+              we make bad Dogs Good
+            </h4>
+            <h4 className='text-justify font-medium text-s xl:text-l'>
+              and Good Dogs Better
+            </h4>
+          </div>
 
-              {/* Buttons */}
-              <div className='flex flex-row justify-between pt-10'>
-                <button className='bg-white text-red w-52 h-14 font-bold rounded-full border border-red drop-shadow-dropred
-                hover:text-white hover:bg-red hover:border-none'>
-                  About Us
-                </button>
+          {/* Buttons */}
+          <div className='flex flex-row justify-between pt-5'>
+            <button className='bg-white text-red w-28 mr-3 lg:w-52 h-9 lg:h-14 font-bold rounded-full border border-red drop-shadow-dropred
+            hover:text-white hover:bg-red hover:border-none'>
+              About Us
+            </button>
 
-                <button className='bg-red text-white w-52 h-14 rounded-full drop-shadow-dropblack font-bold
-                hover:bg-white hover:text-red hover:border hover:border-red hover:rounded-full'>
-                  Contact Us
-                </button>
-              </div>
-            </div>
+            <button className='bg-red text-white w-28 ml-3 lg:w-52 h-9 lg:h-14 rounded-full drop-shadow-dropblack font-bold
+            hover:bg-white hover:text-red hover:border hover:border-red hover:rounded-full'>
+              Contact Us
+            </button>
           </div>
         </div>
       </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,12 @@ import React, {useState, useEffect} from 'react';
 import Image from 'next/image';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import MiscellaneousServicesIcon from '@mui/icons-material/MiscellaneousServices';
+import CollectionsIcon from '@mui/icons-material/Collections';
+import ContactEmergencyIcon from '@mui/icons-material/ContactEmergency';
+import logo from '../public/Logo.png';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -24,15 +30,13 @@ const Navbar = () => {
 
   return (
     <div style={{backgroundColor: `${color}`}} className='fixed left-0 top-0 w-full z-20 bg-navbarbg'>
-      <div className='px-10 h-20 flex flex-row flex-wrap content-center justify-between'>
+      <div className='px-5 h-20 flex flex-row flex-wrap content-center justify-between'>
         {/* Logo */}
         <div>
-          <a href='#' className='block' id='logo' style={{visibility: `${visibility}`}}>
+          <a href='#' className='block w-20 md:w-32' style={{visibility: `${visibility}`}}>
             <Image 
-              src="/Logo.png"
+              src={logo}
               alt="Logo"
-              width="100"
-              height="80"
             />
           </a>
         </div>
@@ -40,16 +44,16 @@ const Navbar = () => {
         {/* Links */}
         <div className='flex flex-row flex-wrap content-center justify-center'>
           <ul className='md:flex flex-row flex-wrap content-center hidden'>
-            <li className='p-5'><a href="#" className='font-medium text-red hover:text-green ease-in duration-300 hover:ease-in hover:duration-300'>HOME</a></li>
-            <li className='p-5'><a href="#" className='font-medium text-red hover:text-green ease-in duration-300 hover:ease-in hover:duration-300'>ABOUT</a></li>
-            <li className='p-5'><a href="#" className='font-medium text-red hover:text-green ease-in duration-300 hover:ease-in hover:duration-300'>SERVICES</a></li>
-            <li className='p-5'><a href="#" className='font-medium text-red hover:text-green ease-in duration-300 hover:ease-in hover:duration-300'>GALLERY</a></li>
-            <li className='p-5'><a href="#" className='font-medium text-red hover:text-green ease-in duration-300 hover:ease-in hover:duration-300'>CONTACT</a></li>
+            <li className='p-5'><a href="#" className='font-medium md:text-xs xl:text-s lg:text-sl text-red hover:text-green ease-in duration-300 hover:ease-in hover:duration-300'>HOME</a></li>
+            <li className='p-5'><a href="#" className='font-medium md:text-xs xl:text-s lg:text-sl text-red hover:text-green ease-in duration-300 hover:ease-in hover:duration-300'>ABOUT</a></li>
+            <li className='p-5'><a href="#" className='font-medium md:text-xs xl:text-s lg:text-sl text-red hover:text-green ease-in duration-300 hover:ease-in hover:duration-300'>SERVICES</a></li>
+            <li className='p-5'><a href="#" className='font-medium md:text-xs xl:text-s lg:text-sl text-red hover:text-green ease-in duration-300 hover:ease-in hover:duration-300'>GALLERY</a></li>
+            <li className='p-5'><a href="#" className='font-medium md:text-xs xl:text-s lg:text-sl text-red hover:text-green ease-in duration-300 hover:ease-in hover:duration-300'>CONTACT</a></li>
           </ul>
         </div> 
 
         {/* Mobile Button */}
-        <div id='ham' onClick={handleNav} className='md:hidden block z-30'>
+        <div id='ham' onClick={handleNav} className='md:hidden block z-30 text-red hover:text-green'>
           {nav ? <CloseIcon /> : <MenuIcon />}
         </div>
 
@@ -57,7 +61,7 @@ const Navbar = () => {
         <div className={
           nav
           ? 'md:hidden fixed top-0 right-0 left-[50%] flex flex-col justify-center items-center w-2/4 h-4/5 bg-navbarbg text-center ease-in duration-500 rounded-bl-3xl'
-          : 'md:hidden absolute top-0 left-[100%] right-[-100%] bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+          : 'md:hidden absolute top-0 left-[100%] right-[-100%] bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-500'
         }>
         
         <a href='#' className='absolute top-[5%]'>
@@ -69,12 +73,13 @@ const Navbar = () => {
           />
         </a>
 
+        {/* Mobile Links */}
         <ul>
-          <li className='p-5'><a href="#" className='font-medium text-red hover:text-green'>HOME</a></li>
-          <li className='p-5'><a href="#" className='font-medium text-red hover:text-green'>ABOUT</a></li>
-          <li className='p-5'><a href="#" className='font-medium text-red hover:text-green'>SERVICES</a></li>
-          <li className='p-5'><a href="#" className='font-medium text-red hover:text-green'>GALLERY</a></li>
-          <li className='p-5'><a href="#" className='font-medium text-red hover:text-green'>CONTACT</a></li>
+          <li className='p-5'><a href="#" className='font-medium text-red hover:text-green'><HomeIcon className='pr-1 w-7' />HOME</a></li>
+          <li className='p-5'><a href="#" className='font-medium text-red hover:text-green'><InfoIcon className='pr-1 w-7' />ABOUT</a></li>
+          <li className='p-5'><a href="#" className='font-medium text-red hover:text-green'><MiscellaneousServicesIcon className='pr-1 w-7' />SERVICES</a></li>
+          <li className='p-5'><a href="#" className='font-medium text-red hover:text-green'><CollectionsIcon className='pr-1 w-7' />GALLERY</a></li>
+          <li className='p-5'><a href="#" className='font-medium text-red hover:text-green'><ContactEmergencyIcon className='pr-1 w-8' />CONTACT</a></li>
         </ul>
         </div>
 
